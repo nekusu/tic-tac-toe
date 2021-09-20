83ba2b9563faef30d2f7e829a2d64e41ac482a96
nekusu <nekusu.dev@gmail.com>
2021-09-20 14:36:43 -0300

    Add winner check

--- js/ticTacToe.js
+++ js/ticTacToe.js
@@ -13,26 +13,49 @@ const boardController = (() => {
 
 	function addMarker(index, marker) {
 		tiles[index].firstElementChild.textContent = markers[marker];
+		tiles[index].classList.add('marked');
 	}
 
-	return { addMarker };
+	function showWinTiles(indexes) {
+		for (const i of indexes) {
+			tiles[i].classList.add('win');
+		}
+	}
+
+	return { addMarker, showWinTiles };
 })();
 
 const gameController = (() => {
 	const board = ['', '', '', '', '', '', '', '', ''];
+	const winCombos = [[0, 1, 2], [3, 4, 5], [6, 7, 8], [0, 3, 6], [1, 4, 7], [2, 5, 8], [0, 4, 8], [2, 4, 6]];
 	const playerOne = Player('x');
 	const playerTwo = Player('o');
 	let currentPlayer = playerOne;
+	let gameFinished = false;
 
 	function switchPlayer() {
 		currentPlayer = currentPlayer === playerOne ? playerTwo : playerOne;
 	}
 
+	function checkWinner() {
+		for (const combo of winCombos) {
+			const [a, b, c] = [board[combo[0]], board[combo[1]], board[combo[2]]];
+			if (a && b && c && a === b && b === c) {
+				gameFinished = true;
+				boardController.showWinTiles(combo);
+				return true;
+			}
+		}
+		return false;
+	}
+
 	function addMarker(index) {
-		if (!board[index]) {
+		if (!board[index] && !gameFinished) {
 			board[index] = currentPlayer.getMarker();
 			boardController.addMarker(index, currentPlayer.getMarker());
-			switchPlayer();
+			if (!checkWinner()) {
+				switchPlayer();
+			}
 		}
 	}
 
